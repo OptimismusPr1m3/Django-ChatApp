8e1916eb8eec7a7140ecf800bd2e6088a60f3a46
Sebastian Wolff <bastiwolff432@gmail.com>
2024-05-28 18:39:50 +0200

    added message model, added ui for chat, migrated message model for db

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
+from django.db.models.fields import DateField
+
 
 # Create your models here.
+
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    create_at = DateField(default=date.today)
+    # chat = Chat Klasse verknüpfen
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set'),
+    reveiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set'),
